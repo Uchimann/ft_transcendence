df74d8ec165f5697c4519fb6b163e4eddfe04b96
Atalay Erdogan <aerdogan@k2m04s01.42kocaeli.com.tr>
2024-09-02 18:47:42 +0300

    push

---
+++ frontend/router.js
@@ -0,0 +1,62 @@
+export async function loadPage(page) {
+    const content = document.getElementById('content');
+    let pageUrl = '';
+    let scriptUrl = '';
+
+    switch (page) {
+        case 'login':
+            pageUrl = 'login/login.html';
+            scriptUrl = 'login/login.js'; // Load the login script
+            break;
+        case 'register':
+            pageUrl = 'register/register.html';
+            scriptUrl = 'register/register.js';
+            break;
+        case 'registerlogin':
+            pageUrl = 'login/login.html';
+            scriptUrl = 'register/register.js';
+            break
+        case 'forgot':
+            pageUrl = 'forgot/forgot.html';
+            break;
+        case 'profile':
+            pageUrl = 'profile/profile.html';
+            break;
+        case 'about':
+            pageUrl = 'about/about.html';
+            break;
+        case 'logout':
+            pageUrl = 'login/login.html';
+            scriptUrl = 'login/login.js'; // Load the login script again
+            break;
+        case 'settings':
+            pageUrl = 'settings/settings.html';
+            break;
+        case 'game':
+            pageUrl = 'game/game.html';
+            break;
+        case 'send':
+            pageUrl = 'forgot/forgot.html';
+            break;
+        default:
+            pageUrl = '404/404.html';
+    }
+
+    try {
+        const response = await fetch(pageUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to load page: ${response.statusText}`);
+        }
+        const html = await response.text();
+        content.innerHTML = html;
+
+        if (scriptUrl) {
+            const script = document.createElement('script');
+            script.src = scriptUrl;
+            document.body.appendChild(script);
+        }
+
+    } catch (error) {
+        content.innerHTML = `<p>Error loading page: ${error.message}</p>`;
+    }
+}
